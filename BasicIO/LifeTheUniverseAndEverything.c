b2fcc8458f236226093a928399295227ebf0d33b
Digres45 <digresh.sawale111@gmail.com>
2017-07-27 00:36:34 +0530

    Create LifeTheUniverseAndEverything.c

---
+++ BasicIO/LifeTheUniverseAndEverything.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int no;
+    
+    while(1){
+        
+        scanf("%d",&no);
+        
+        if(no==42) break;
+        
+        else{
+            printf("%d\n",no);
+        }
+    }
+    
+    return 0;
+}
